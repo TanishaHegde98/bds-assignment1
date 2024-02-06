3afa790010270ecbc02bd81da713de403ffbb608
abolipai-wisc <apai7@wisc.edu>
2024-02-06 17:56:28 -0600

    Optimizing code

--- PART3/Task1/pagerank_task1.py
+++ PART3/Task1/pagerank_task1.py
@@ -13,15 +13,21 @@ def create_spark_application(input_file_path, output_file_path):
     # Filter out the rows that are comments and start with "#"
     filtered_rdd = df_rdd.filter(lambda line: line[0] != "#")
 
-    # Get the first value from the line and store distinct values as nodes in the page rank algorithm
+    ### The following code added more rdds and redundant computation for finding the distinct nodes
+    '''
+    #Get the first value from the line and store distinct values as nodes in the page rank algorithm
     node_rdd = filtered_rdd.map(lambda line: line.split("\t")[0]).distinct()
 
-    # Initialize a rank for each node as 1
+    #Initialize a rank for each node as 1
     rank = node_rdd.map(lambda node: (node, 1))
+    '''
 
     # Split every line by tab and get the first and second value as the source and destination nodes
     # Group by key to get the destination nodes for each source node
     links = filtered_rdd.map(lambda line: (line.split("\t")[0],line.split("\t")[1])).groupByKey()
+
+    # Get the distinct nodes from the links rdd and initialize a rank for each node as 1
+    rank = links.map(lambda node: (node[0], 1))
 
     # Run the page rank algorithm for 10 iterations
     for i in range(10):
@@ -31,11 +37,8 @@ def create_spark_application(input_file_path, output_file_path):
         contributions_per_page = links.join(rank).flatMap(lambda val: [(page, val[1][1]/len(val[1][0])) for page in val[1][0]])
 
         # Sum the contributions per page received from other nodes
-        sum_contributions_per_page = contributions_per_page.reduceByKey(lambda a, b: a + b)
+        rank = contributions_per_page.reduceByKey(lambda a, b: a + b).mapValues(lambda contribution: 0.15 + 0.85 * contribution)
 
-        # Calculate the new rank for each node by using the formula
-        rank = sum_contributions_per_page.mapValues(lambda contribution: 0.15 + 0.85 * contribution)
-    
     # Store the final rank in the output file
     rank.saveAsTextFile(output_file_path)
 
