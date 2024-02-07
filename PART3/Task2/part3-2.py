836b2d7952bf4c2957305a90be6c26d743672a03
abolipai-wisc <apai7@wisc.edu>
2024-02-07 13:33:35 -0600

    Making RDD names consistent

--- PART3/Task2/part3-2.py
+++ PART3/Task2/part3-2.py
@@ -13,22 +13,30 @@ def create_spark_application(input_file_path, output_file_path):
     print("Initial Number of Partions: ", df_rdd.getNumPartitions())
     #df_rdd = df_rdd.repartition(numPartitions)
     #df.printSchema()
+
+    # Filter out the rows that are comments and start with "#"
     filtered_rdd = df_rdd.filter(lambda line: line[0] != "#")
+
+    ### The following code added more rdds and redundant computation for finding the distinct nodes
+
+    #Get the first value from the line and store distinct values as nodes in the page rank algorithm
     node_rdd = filtered_rdd.map(lambda line: line.split("\t")[0]).distinct()
-    node_rank = node_rdd.map(lambda node: (node, 1))
 
-    node_pages = filtered_rdd.map(lambda line: (line.split("\t")[0],line.split("\t")[1])).groupByKey().repartition(numPartitions)
-    print(node_pages.getNumPartitions())
+    #Initialize a rank for each node as 1
+    rank = node_rdd.map(lambda node: (node, 1))
+
+    # Split every line by tab and get the first and second value as the source and destination nodes
+    # Group by key to get the destination nodes for each source node
+    links = filtered_rdd.map(lambda line: (line.split("\t")[0],line.split("\t")[1])).groupByKey().repartition(numPartitions)
+
+    print(links.getNumPartitions())
     for i in range(10):
-        contributions_per_page = node_pages.join(node_rank).flatMap(lambda val: [(page, val[1][1]/len(val[1][0])) for page in val[1][0]])
+        contributions_per_page = links.join(rank).flatMap(lambda val: [(page, val[1][1]/len(val[1][0])) for page in val[1][0]])
         sum_contributions_per_page = contributions_per_page.reduceByKey(lambda a, b: a + b)
-        node_rank = sum_contributions_per_page.mapValues(lambda contribution: 0.15 + 0.85 * contribution)
+        rank = sum_contributions_per_page.mapValues(lambda contribution: 0.15 + 0.85 * contribution)
     
-    node_rank_10 = node_rank.take(10)
-    for node in node_rank_10:
-        print(node)
 
-    node_rank.coalesce(1).saveAsTextFile(output_file_path)
+    rank.coalesce(1).saveAsTextFile(output_file_path)
 
 def main():
     if len(sys.argv) < 3:
